980234d7f0061249551fe523652c7c04ae9f25a7
DanielAGBIDINOUKOUN <danielagbidinoukoun26@gmail.com>
2025-02-18 08:08:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,161 @@
+const sr = ScrollReveal();
+
+// Configurez les options d'animation
+sr.reveal('.header__container .hello', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'bottom',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 200,  // Délai avant l'animation
+});
+
+sr.reveal('.header__container h1', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'bottom',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 500,  // Délai avant l'animation
+});
+
+sr.reveal('.header__container .section__title ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'bottom',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 1000,  // Délai avant l'animation
+});
+
+sr.reveal('.header__container .bien', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'left',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 1200,  // Délai avant l'animation
+});
+
+sr.reveal('.header__container .video', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'right',  // Point d'origine de l'animation
+    distance: '70px',  // Distance de déplacement
+    delay: 1400,  // Délai avant l'animation
+});
+
+// section 2
+
+sr.reveal('.about__image ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'left',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 600,  // Délai avant l'animation
+});
+
+
+sr.reveal('.about__content .section__title ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 700,  // Délai avant l'animation
+});
+
+sr.reveal('.about__content .section__subtitle ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'right',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 700,  // Délai avant l'animation
+});
+sr.reveal('.about__content .about__details ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'bottom',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 800,  // Délai avant l'animation
+});
+// section 3
+
+sr.reveal('.service__container .section__subtitle ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 800,  // Délai avant l'animation
+});
+
+sr.reveal('.service__container h2 ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 800,  // Délai avant l'animation
+});
+
+sr.reveal('#anicard1 ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 500,  // Délai avant l'animation
+});
+
+sr.reveal('#anicard2 ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 800,  // Délai avant l'animation
+});
+
+sr.reveal('#anicard3 ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 1100,  // Délai avant l'animation
+});
+
+// section 4
+
+sr.reveal('#anicard1 ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 500,  // Délai avant l'animation
+});
+
+sr.reveal('#anicard2 ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 800,  // Délai avant l'animation
+});
+
+sr.reveal('#anicard3 ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 1100,  // Délai avant l'animation
+});
+
+sr.reveal('#anicard4 ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 1400,  // Délai avant l'animation
+});
+
+sr.reveal('#anicard5 ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 1700,  // Délai avant l'animation
+});
+
+sr.reveal('#anicard6 ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'top',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 2000,  // Délai avant l'animation
+});
+
+sr.reveal('.project__container .section__subtitle ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'left',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 800,  // Délai avant l'animation
+});
+
+sr.reveal('.project__container h2 ', {
+    duration: 1000,  // Durée de l'animation en millisecondes
+    origin: 'left',  // Point d'origine de l'animation
+    distance: '50px',  // Distance de déplacement
+    delay: 800,  // Délai avant l'animation
+});
